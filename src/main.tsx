66f0e114fbce1211c8837553a59d2904340ba1af
Tomas Pozo <tomaspozogarzon@gmail.com>
2024-11-29 23:46:18 -0500

    Set default theme to dark

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReduxProvider store={store}>
       <NuqsAdapter>
-        <ThemeProvider attribute="class">
+        <ThemeProvider attribute="class" defaultTheme="dark">
           <App />
         </ThemeProvider>
       </NuqsAdapter>
